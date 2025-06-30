d7f7b97b6d99bfa828a61fbceb38b04d8b65e623
zengks <zengks@outlook.com>
2025-06-30 16:20:00 -0700

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,27 @@ import EducationSection from "./components/EducationSection";
 
 export default function Home() {
   return (
-    <div className="glass-container outer-glass-container">
-      {/* <div className="absolute inset-0 bg-white/10 backdrop-blur-2xl" /> */}
-      <NavBar />
-      <main className="flex">
-        <section style={{ border: "1px solid red" }} className="flex-10/100">
-          <SideBar />
-        </section>
-        <section style={{ border: "1px solid red" }} className="flex-90/100">
-          <UserAbout />
-          <UserSkillSet />
-          <WorkExpSection />
-          <EducationSection />
-        </section>
-      </main>
-    </div>
+    <>
+      <div className="glass-container outer-glass-container">
+        {/* <div className="absolute inset-0 bg-white/10 backdrop-blur-2xl" /> */}
+        <NavBar />
+        <main className="flex">
+          <section style={{ border: "1px solid red" }} className="flex-10/100">
+            <SideBar />
+          </section>
+          <section style={{ border: "1px solid red" }} className="flex-90/100">
+            <UserAbout />
+            <UserSkillSet />
+            <WorkExpSection />
+            <EducationSection />
+          </section>
+        </main>
+      </div>
+      <footer className="glass-footer">
+        <div className="text-center">
+          © {new Date().getFullYear()} Steven Zeng. All Rights Reserved
+        </div>
+      </footer>
+    </>
   );
 }
