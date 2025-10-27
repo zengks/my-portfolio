33a86522756ee5c8ab754f018f4ca7250d18db99
zengks <zengks@outlook.com>
2025-10-26 19:04:10 -0700

    added a bunch of skill branch svg icons

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,4 +1,27 @@
-import nextjsLogo from '@/assets/icons/nextjs.svg';
+import NextJSIcon from '@/assets/icons/nextjs.svg';
+import AngularIcon from '@/assets/icons/angular.svg';
+import ReactIcon from '@/assets/icons/react.svg';
+import TypeScriptIcon from '@/assets/icons/typescript.svg';
+import JavaScriptIcon from '@/assets/icons/javascript.svg';
+import HTML5Icon from '@/assets/icons/html5.svg';
+import CSSIcon from '@/assets/icons/css.svg';
+import NodeJSIcon from '@/assets/icons/nodejs.svg';
+import ExpressIcon from '@/assets/icons/express.svg';
+import MongoDBIcon from '@/assets/icons/mongodb.svg';
+import PostgreSQLIcon from '@/assets/icons/postgresql.svg';
+import VueIcon from '@/assets/icons/vue.svg';
+import DockerIcon from '@/assets/icons/docker.svg';
+import djangoIcon from '@/assets/icons/django.svg';
+import githubIcon from '@/assets/icons/github.svg';
+import prismaIcon from '@/assets/icons/prisma.svg';
+import bootstrapIcon from '@/assets/icons/bootstrap.svg';
+import tailwindcssIcon from '@/assets/icons/tailwindcss.svg';
+import vercelIcon from '@/assets/icons/vercel.svg';
+import gitlabIcon from '@/assets/icons/gitlab.svg';
+import swiftIcon from '@/assets/icons/swift.svg';
+import confluenceIcon from '@/assets/icons/confluence.svg';
+import jiraIcon from '@/assets/icons/jira.svg';
+import wordpressIcon from '@/assets/icons/wordpress.svg';
 
 export const MENU_ITEMS = [
 	{
@@ -27,51 +50,136 @@ export const MENU_ITEMS = [
 	},
 ];
 
-export const SKILLS = [
+export const WEB_DEV_SKILLS = [
 	{
-		name: 'FULL-STACK',
-		logo: [
-			{ logoSrc: nextjsLogo, logoAlt: 'NextJS Icon' },
-			{ logoSrc: nextjsLogo, logoAlt: 'NextJS Icon' },
-			{ logoSrc: nextjsLogo, logoAlt: 'NextJS Icon' },
-		],
-		description: 'This is NEXTJS full stack',
+		name: 'Next JS',
+		src: NextJSIcon,
+		altText: 'NextJS Icon',
 	},
 	{
-		name: 'Next.JS',
-		logo: [
-			{ logoSrc: nextjsLogo, logoAlt: 'NextJS Icon' },
-			{ logoSrc: nextjsLogo, logoAlt: 'NextJS Icon' },
-			{ logoSrc: nextjsLogo, logoAlt: 'NextJS Icon' },
-			{ logoSrc: nextjsLogo, logoAlt: 'NextJS Icon' },
-			{ logoSrc: nextjsLogo, logoAlt: 'NextJS Icon' },
-		],
-		description: 'This is NEXTJS icon',
+		name: 'React JS',
+		src: ReactIcon,
+		altText: 'React Icon',
 	},
 	{
-		name: 'Next.JS',
-		logo: [{ logoSrc: nextjsLogo, logoAlt: 'NextJS Icon' }],
-		description: 'This is NEXTJS icon',
+		name: 'Angular JS',
+		src: AngularIcon,
+		altText: 'Angular Icon',
 	},
 	{
-		name: 'Next.JS',
-		logo: [{ logoSrc: nextjsLogo, logoAlt: 'NextJS Icon' }],
-		description: 'This is NEXTJS icon',
-	},
-	,
-	{
-		name: 'Next.JS',
-		logo: [{ logoSrc: nextjsLogo, logoAlt: 'NextJS Icon' }],
-		description: 'This is NEXTJS icon',
+		name: 'Vue JS',
+		src: VueIcon,
+		altText: 'Vue Icon',
 	},
 	{
-		name: 'Next.JS',
-		logo: [{ logoSrc: nextjsLogo, logoAlt: 'NextJS Icon' }],
-		description: 'This is NEXTJS icon',
+		name: 'TypeScript',
+		src: TypeScriptIcon,
+		altText: 'TypeScript Icon',
 	},
 	{
-		name: 'Next.JS',
-		logo: [{ logoSrc: nextjsLogo, logoAlt: 'NextJS Icon' }],
-		description: 'This is NEXTJS icon',
+		name: 'JavaScript',
+		src: JavaScriptIcon,
+		altText: 'JavaScript Icon',
+	},
+	{
+		name: 'WordPress',
+		src: wordpressIcon,
+		altText: 'WordPress Icon',
+	},
+	{
+		name: 'HTML 5',
+		src: HTML5Icon,
+		altText: 'HTML5 Icon',
+	},
+	{
+		name: 'CSS 3',
+		src: CSSIcon,
+		altText: 'CSS3 Icon',
+	},
+	{
+		name: 'Node JS',
+		src: NodeJSIcon,
+		altText: 'NodeJS Icon',
+	},
+	{
+		name: 'Express JS',
+		src: ExpressIcon,
+		altText: 'ExpressJS Icon',
+	},
+	{
+		name: 'MongoDB',
+		src: MongoDBIcon,
+		altText: 'MongoDB Icon',
+	},
+	{
+		name: 'PostgreSQL',
+		src: PostgreSQLIcon,
+		altText: 'PostgreSQL Icon',
+	},
+	{
+		name: 'Docker',
+		src: DockerIcon,
+		altText: 'Docker Icon',
+	},
+	{
+		name: 'Prisma',
+		src: prismaIcon,
+		altText: 'Prisma Icon',
+	},
+	{
+		name: 'Tailwind CSS',
+		src: tailwindcssIcon,
+		altText: 'Tailwind CSS Icon',
+	},
+	{
+		name: 'Bootstrap',
+		src: bootstrapIcon,
+		altText: 'Bootstrap Icon',
+	},
+	{
+		name: 'Vercel',
+		src: vercelIcon,
+		altText: 'Vercel Icon',
+	},
+	{
+		name: 'Django',
+		src: djangoIcon,
+		altText: 'Django Icon',
+	},
+];
+
+export const MOBILE_DEV_SKILLS = [
+	{
+		name: 'React Native',
+		src: ReactIcon,
+		altText: 'React Icon',
+	},
+	{
+		name: 'Swift',
+		src: swiftIcon,
+		altText: 'Swift Icon',
+	},
+];
+
+export const PROJECT_MANAGEMENT_TOOLS = [
+	{
+		name: 'GitHub',
+		src: githubIcon,
+		altText: 'GitHub Icon',
+	},
+	{
+		name: 'GitLab',
+		src: gitlabIcon,
+		altText: 'GitLab Icon',
+	},
+	{
+		name: 'Jira',
+		src: jiraIcon,
+		altText: 'Jira Icon',
+	},
+	{
+		name: 'Confluence',
+		src: confluenceIcon,
+		altText: 'Confluence Icon',
 	},
 ];
