223c733ade6ce79a597cd2b50dc9a1c41b97b36c
zengks <zengks@outlook.com>
2025-11-02 18:34:46 -0800

    added accordion component for projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,3 +1,22 @@
+import ProjectAccordion from '../components/ProjectAccordion';
+import SideBar from '../components/SideBar';
+
+import { Suspense } from 'react';
+
 export default function Projects() {
-  return <div>Projects</div>;
+	return (
+		<main className="flex">
+			<section className="w-1/4">
+				<Suspense fallback={<div>Loading...</div>}>
+					<SideBar />
+				</Suspense>
+			</section>
+			<section className="w-3/4">
+				<p>Work History</p>
+				<Suspense fallback={<div>Loading...</div>}>
+					<ProjectAccordion />
+				</Suspense>
+			</section>
+		</main>
+	);
 }
