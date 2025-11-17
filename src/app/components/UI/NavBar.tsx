df00055b6e82131d437f6e46c1ae1bfe9df02041
zengks <zengks@outlook.com>
2025-11-16 23:28:45 -0800

    optimized menu bar item style

--- src/app/components/UI/NavBar.tsx
+++ src/app/components/UI/NavBar.tsx
@@ -1,23 +1,45 @@
+'use client';
+
 import { MENU_ITEMS } from '@/lib/constant';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 import moonIcon from 'src/assets/icons/moon.svg';
 import sunIcon from 'src/assets/icons/sun.svg';
 import Image from 'next/image';
 
 export default function NavBar() {
+	const pathname = usePathname();
+
 	return (
 		<div className="flex justify-around items-center my-8">
 			<Link href="/" className="tracking-[16]">
-				<p className="logo logo-creative0 ps-[40]">steven</p>
+				<p className="logo logo-creative ps-[40]">steven</p>
 			</Link>
 			<div className="flex justify-between items-center gap-10">
 				<nav className="flex justify-center gap-9 text-[15px] items-center">
-					{MENU_ITEMS.map((item) => (
-						<Link key={item.name} href={item.link}>
-							{item.name.toUpperCase()}
-						</Link>
-					))}
+					{MENU_ITEMS.map((item) => {
+						const isActive = pathname === item.link;
+						return (
+							<Link
+								key={item.name}
+								href={item.link}
+								className="group relative pb-1 text-neutral-600 hover:text-neutral-900 transition-colors"
+							>
+								{item.name.toUpperCase()}
+
+								<span
+									className={`
+                    absolute bottom-0 left-1/2 -translate-x-1/2
+                    h-[2px] bg-neutral-700 rounded-full
+                    transition-all duration-400 ease-out
+                    ${isActive ? 'w-[8px]' : 'w-0'}
+                    ${'group-hover:w-full'}
+                  `}
+								/>
+							</Link>
+						);
+					})}
 				</nav>
 			</div>
 			<div className="flex justify-center items-center gap-2 me-4">
